297c2b17d6f8367c7e0ba7ab007c19acc274c8ed
Jeff Caldwell <nemo.omen@gmail.com>
2021-02-12 18:38:13 -0600

    I don't remember

--- Assignment03/Vehicles/src/Vehicle.java
+++ Assignment03/Vehicles/src/Vehicle.java
@@ -1,3 +1,42 @@
 public abstract class Vehicle {
+  String name;
+  int numberOfEngines;
+  int numberOfPassengers;
+  String whereToDrive;
+
+  //getters and setters
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getNumberOfEngines() {
+    return numberOfEngines;
+  }
+
+  public void setNumberOfEngines(int numberOfEngines) {
+    this.numberOfEngines = numberOfEngines;
+  }
+
+  public int getNumberOfPassengers() {
+    return numberOfPassengers;
+  }
+
+  public void setNumberOfPassengers(int numberOfPassengers) {
+    this.numberOfPassengers = numberOfPassengers;
+  }
+
+  public String getWhereToDrive() {
+    return whereToDrive;
+  }
+
+  public void setWhereToDrive(String whereToDrive) {
+    this.whereToDrive = whereToDrive;
+  }
+
   
 }
